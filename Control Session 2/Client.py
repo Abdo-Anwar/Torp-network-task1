ddb489a829651935eb596caed5087899702299f2
Anwar <abd.ahm.anwar@gmail.com>
2024-09-04 22:38:45 +0300

    when enter any msg in client execpt exit the clinet send request to the server and then the server send the Info packeet num

--- Control Session 2/Client.py
+++ Control Session 2/Client.py
@@ -1,4 +1,6 @@
 import socket as sk
+global Pk_Num
+Pk_Num= 0
 
 
 server_ip='192.168.1.26'
@@ -7,9 +9,13 @@ sock=sk.socket(sk.AF_INET,sk.SOCK_STREAM)
 sock.connect((server_ip,server_port))
 while True:
     msg=input("Enter message: ")
-    sock.send(msg.encode('utf-8'))
+    
+   
+    sock.send(Pk_Num.to_bytes(4, 'big'))
     data=sock.recv(1024)
     print(data.decode('utf-8'))
+    Pk_Num += 1
+
     if msg=='exit':
         print("exiting")
         break
--- Control Session 2/Server.py
+++ Control Session 2/Server.py
@@ -2,6 +2,7 @@ import socket
 
 HOST = socket.gethostbyname(socket.gethostname())
 PORT = 12345
+Pk_Num= 0
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((HOST, PORT))
@@ -9,18 +10,25 @@ server_socket.listen(5)
 print(f"Server listening on {HOST}:{PORT}")
 
 while True:
-        global data
-        client_socket, client_address = server_socket.accept()
-        print(f"Connection from {client_address}")
-        while True:
-                data = client_socket.recv(1024)
-                print(data.decode())
-                if data.decode()=='exit':
-                        break
-                else:
-                        client_socket.send(data)
-        if data.decode()=='exit':
-                break
-        
+     
+    client_socket, client_address = server_socket.accept()
+    print(f"Connection from {client_address}")
 
-client_socket.close()
+    while True:   
+        packet_num_bytes = client_socket.recv(4)
+        if not packet_num_bytes:
+            break
+
+        packet_num = int.from_bytes(packet_num_bytes, 'big')
+        print(f"Received packet number: {packet_num}")
+        
+        response = f"Packet {packet_num} received"
+        client_socket.send(response.encode('utf-8'))
+
+        if packet_num == 10:
+            break
+    
+    client_socket.close()
+
+
+server_socket.close()
